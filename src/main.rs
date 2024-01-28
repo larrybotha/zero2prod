8f892d50c2c3a489fff61c6f18c922f3a3b819b1
Larry Botha <lawrenceguy@gmail.com>
2024-01-28 11:02:02 +0200

    feat(startup.rs): use a pool for database connections

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 //! src/main.rs
 
+use sqlx::PgPool;
 use zero2prod::configuration;
 use zero2prod::startup;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = configuration::get_configuration().expect("Failed to read configuration");
+    let pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to database");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     // Create a socket that is bound to a port on the host
     // This listener is the server, and client requests made to localhost:8000 will
     // be handled by this server
     let listener = std::net::TcpListener::bind(address)?;
 
-    startup::run(listener)?.await
+    startup::run(listener, pool)?.await
 }
