cf2f49529206ad439ff8efa3be74fa4d1c6f84b9
Larry Botha <lawrenceguy@gmail.com>
2024-02-10 14:47:41 +0200

    feat(subscriptions.rs): enable tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,6 +13,32 @@ pub struct FormData {
 // T in Form<T> must implement DeserializeOwned from serde, which will deserialize
 // a url-encoded string into that struct
 pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    let request_prefix = format!("[request_id {request_id}] - ");
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber",
+        // The '%' indicates to `tracing` that we want Display used to render the
+        // values
+        request_id = %request_id, // equivalent to request_id = %request_id
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+    // After creating a span
+    //
+    // .enter() shouldn't be used in async code!
+    let _request_span_guard = request_span.enter();
+
+    tracing::info!(
+        "{}'{}' '{}' as a new subscriber",
+        request_prefix,
+        form.email,
+        form.name
+    );
+    tracing::info!(
+        "{} Saving new subscriber details in database",
+        request_prefix
+    );
+
     let result = sqlx::query!(
         r#"
             INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -27,9 +53,14 @@ pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) ->
     .await;
 
     match result {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!("{}Subscriber saved to database", request_prefix);
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            // use Debug to log the error - we want more information in the logs
+            // than Display would show
+            tracing::error!("{}Failed to execute query: {:?}", request_prefix, e);
             HttpResponse::InternalServerError().finish()
         }
     }
